a69c4d1a0a8b54fec513f8e004b074b2c971aea7
Leon Leiker <leonleiker13@gmail.com>
2025-07-17 18:28:08 +0200

    Add: Create UI-Layout

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,51 +6,125 @@
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true" class="ion-padding">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-content class="ion-padding">
+      <ion-list lines="none">
+        <ion-item>
+          <ion-select
+            v-model="sourceLanguage"
+            interface="popover"
+            placeholder="Auswahl Quellsprache"
+          >
+            <ion-select-option value="de">Deutsch</ion-select-option>
+            <ion-select-option value="en">Englisch</ion-select-option>
+            <ion-select-option value="fr">Französisch</ion-select-option>
+            <ion-select-option value="it">Italienisch</ion-select-option>
+            <ion-select-option value="es">Spanisch</ion-select-option>
+          </ion-select>
+        </ion-item>
+      </ion-list>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <ion-item lines="none">
+        <ion-textarea
+          v-model="sourceText"
+          placeholder="Text hier eingeben"
+          auto-grow
+        ></ion-textarea>
+      </ion-item>
+
+      <ion-list lines="none" class="top-gap">
+        <ion-item>
+          <ion-select
+            v-model="targetLanguage"
+            interface="popover"
+            placeholder="Auswahl Zielsprache"
+          >
+            <ion-select-option value="de">Deutsch</ion-select-option>
+            <ion-select-option value="en">Englisch</ion-select-option>
+            <ion-select-option value="fr">Französisch</ion-select-option>
+            <ion-select-option value="it">Italienisch</ion-select-option>
+            <ion-select-option value="es">Spanisch</ion-select-option>
+          </ion-select>
+        </ion-item>
+      </ion-list>
+
+      <ion-item lines="none">
+        <ion-textarea
+          readonly
+          v-model="translatedText"
+          placeholder="Übersetzung"
+          auto-grow
+        ></ion-textarea>
+      </ion-item>
+
+      <ion-button expand="block" class="top-gap" @click="copyToClipboard">
+        Kopieren
+      </ion-button>
+      <ion-button expand="block" @click="speakText">
+        Sprachausgabe
+      </ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { ref, watch } from 'vue'
+import {
+  IonPage, IonHeader, IonToolbar, IonTitle, IonContent,
+  IonList, IonItem, IonSelect, IonSelectOption,
+  IonTextarea, IonButton, loadingController, toastController
+} from '@ionic/vue'
+
+const sourceLanguage = ref('')
+const targetLanguage = ref('')
+const sourceText = ref('')
+const translatedText = ref('')
+
+function debounce(fn: (...a: any[]) => any, d = 300) {
+  let t: ReturnType<typeof setTimeout> | undefined
+  return (...a: any[]) => {
+    clearTimeout(t)
+    t = setTimeout(() => fn(...a), d)
+  }
+}
+
+async function translate(text: string, from: string, to: string) {
+  await new Promise(r => setTimeout(r, 400))
+  return `[${from}→${to}] ${text}`
+}
+
+const debouncedTranslate = debounce(async () => {
+  if (!sourceText.value || !sourceLanguage.value || !targetLanguage.value) {
+    translatedText.value = ''
+    return
+  }
+  const loader = await loadingController.create({ message: 'Übersetze …' })
+  await loader.present()
+  try {
+    translatedText.value = await translate(
+      sourceText.value,
+      sourceLanguage.value,
+      targetLanguage.value
+    )
+  } catch {
+    const toast = await toastController.create({
+      message: 'Übersetzung fehlgeschlagen',
+      color: 'danger',
+      duration: 2500
+    })
+    toast.present()
+  } finally {
+    loader.dismiss()
+  }
+}, 300)
+
+watch([sourceText, sourceLanguage, targetLanguage], debouncedTranslate)
+
+async function copyToClipboard () {}
+async function speakText () {}
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+.top-gap {
+  margin-top: 24px;
 }
 </style>
